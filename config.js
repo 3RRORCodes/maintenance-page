25f324d2d1b8a053b6440d74239d5f8c456abee0
3RRORCodes <a.aryantsk12@gmail.com>
2024-10-17 22:38:21 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    title: "Error Hub - Under Maintenance",
+    mainHeading: "Error Hub is Leveling Up!",
+    mainText: "We're working on some exciting new features to enhance your Error Hub experience. Stay tuned— we'll be back soon, better than ever!",
+    storeText: "In the meantime, check out our store for exclusive treasures.",
+    footerText: "Error Hub 2024. All Rights Reserved.",
+    discordLink: "https://discord.gg/Eux7CJEQN2",
+    youtubeLink: "https://www.youtube.com/@ERRORHub",
+    docsLink: "https://docs.errorhub.co",
+    storeLink: "https://errorhub.tebex.io"
+};
